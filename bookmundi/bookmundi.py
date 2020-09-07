858ee2a67f3d314e78fb5280b8cd8cb1306fbab3
Sudan <sudan1bhandari@gmail.com>
2020-09-07 20:42:20 +0545

    pagination

--- bookmundi/bookmundi.py
+++ bookmundi/bookmundi.py
@@ -4,24 +4,27 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-url="https://www.bookmundi.com/nepal"
+visiting_nepal = []
+for x in range(1,45):
+    url="https://www.bookmundi.com/nepal?page="
+    r=  requests.get(url+str(x))
 
-r=  requests.get(url)
+    htmlcontent=r.content
 
-htmlcontent=r.content
+    soup=BeautifulSoup(htmlcontent,'html.parser')
 
-soup=BeautifulSoup(htmlcontent,'html.parser')
+    placelist=soup.find(id='tours-main-holder')
+    place=placelist.find_all(class_='text-details')
+    print(place[0].find('a',class_="target-link").get_text())
 
-placelist=soup.find(id='tours-main-holder')
-place=placelist.find_all(class_='details-block')
-print(place[0].find(class_="target-link").get_text())
+    placename=[places.find('a',class_="target-link").get_text() for places in place]
 
-placename=[places.find(class_='target-link').get_text() for places in place]
-print(placename)
 
-bookmundi=pd.DataFrame({
-    'placenamelist':placename,
-})
-print(bookmundi)
+    bookmundi={
+        'Thay':placename
+    }
+    visiting_nepal.append(bookmundi)
 
-bookmundi.to_csv('bookmundi/bookmundi.csv')
+
+df=pd.DataFrame(visiting_nepal)
+df.to_csv('bookmundi/bookmundi.csv')
