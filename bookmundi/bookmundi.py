2da3f571b9b0f6a1ac98c3a06f36c48b3bb8097f
Sudan <sudan1bhandari@gmail.com>
2020-09-08 20:56:06 +0545

    all list are added

--- bookmundi/bookmundi.py
+++ bookmundi/bookmundi.py
@@ -4,25 +4,29 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-visiting_nepal = []
+visitingnepal = [ ]
 for x in range(1,45):
     url="https://www.bookmundi.com/nepal?page="
     r=  requests.get(url+str(x))
+
     htmlcontent=r.content
+
     soup=BeautifulSoup(htmlcontent,'html.parser')
+
     placelist=soup.find(id='tours-main-holder')
-    place=placelist.find_all(class_='text-details')
+    place=placelist.find_all(class_='details-block')
     for places in place:
         placename=places.find('h4').get_text()
+        print(placename)
         tourtype=places.find(class_="txt").get_text()
 
+        bookmundi ={
+            'Place': placename,
+            'TourType': tourtype
+        }
+        visitingnepal.append(bookmundi)
+    print(visitingnepal)
 
-    bookmundi={
-        'Thay':placename,
-        'tour':tourtype
-    }
-    visiting_nepal.append(bookmundi)
 
-
-df=pd.DataFrame(visiting_nepal)
+df=pd.DataFrame(visitingnepal)
 df.to_csv('bookmundi/bookmundi.csv')
